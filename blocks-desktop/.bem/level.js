443aa39e1393f4f00a8dabe2e4cb078f49d167f6
Varvara Stepanova <toivonen@toivonen.ru>
2011-07-18 18:36:18 +0400

    Shortcut for bemhtml technology is useless

--- blocks-desktop/.bem/level.js
+++ blocks-desktop/.bem/level.js
@@ -2,6 +2,5 @@ var myPath = require('bem/lib/path');
 
 exports.techs = {
     'js':        '../i-bem/bem/techs/js.js',
-    'css':       'bem/lib/techs/css',
-    'bemhtml':        '../i-bem/bem/techs/bemhtml.js',
+    'css':       'bem/lib/techs/css'
 };
