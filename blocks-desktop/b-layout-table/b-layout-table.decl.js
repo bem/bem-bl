5eb4abc992c4cc5b70dbca1d737dde6b9bf972d9
Varvara Stepanova <toivonen@yandex-team.ru>
2011-08-09 13:35:55 +0400

    b-layout-table: Block for layout using tag table

---
+++ blocks-desktop/b-layout-table/b-layout-table.decl.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "name": "b-layout-table",
+    "techs": [
+        {
+            "name": "decl.js"
+        },
+        {
+            "name": "css"
+        },
+        {
+            "name": "bemhtml"
+        }
+    ],
+    "elems": [
+        {
+            "name": "gap",
+            "techs": [
+                {
+                    "name": "bemhtml"
+                }
+            ]
+        }
+    ]
+};
