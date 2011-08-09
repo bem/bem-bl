e6503ae07debfabb63d86dc3161d273c7f1e0f29
Varvara Stepanova <toivonen@yandex-team.ru>
2011-08-09 13:35:14 +0400

    b-icon

---
+++ blocks-desktop/b-icon/b-icon.decl.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "name": "b-icon",
+    "techs": [
+        {
+            "name": "decl.js"
+        },
+        {
+            "name": "bemhtml"
+        },
+        {
+            "name": "css"
+        }
+    ]
+};
