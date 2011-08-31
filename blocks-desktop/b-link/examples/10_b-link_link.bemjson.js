111f2c7646c737d42e3d7b88ba87704b50229ad7
Varvara Stepanova <toivonen@yandex-team.ru>
2011-08-31 18:10:01 +0400

    b-link: Use i-jquery__core in example

--- blocks-desktop/b-link/examples/10_b-link_link.bemjson.js
+++ blocks-desktop/b-link/examples/10_b-link_link.bemjson.js
@@ -3,6 +3,7 @@
     title: 'b-link',
     head: [
         { elem: 'css', url: '_10_b-link_link.css' },
+        { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: '_10_b-link_link.js' }
     ],
     content: [
