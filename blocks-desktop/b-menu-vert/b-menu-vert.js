00c7301f24ef9308faa16b74814aaf6c8d71d21d
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-13 19:25:01 +0400

    b-menu-vert: Vertical menus

--- blocks-desktop/b-menu-vert/b-menu-vert.js
+++ blocks-desktop/b-menu-vert/b-menu-vert.js
@@ -3,40 +3,10 @@
 
 (function() {
 
-BEM.DOM.decl('b-menu-vert', {
-
-    onElemSetMod : {
-
-        'trigger' : {
-
-            'state' : function(elem, modName, modVal) {
-
-                this
-                .toggleMod(
-                    this.findElem(elem.closest(this.buildSelector('layout-unit')), 'item-content').eq(0),
-                    'visibility',
-                    'visible',
-                    modVal == 'opened')
-                .trigger('trigger', {
-                    domElem : elem,
-                    state : modVal
-                });
-            }
-
-        }
-
-    },
-    onTriggerClick: function(e) {
-        this.toggleMod(e.data.domElem, 'state', 'opened');
-    }
-
-}, {
+BEM.DOM.decl({ name: 'b-menu-vert', baseBlock: 'i-menu' }, {}, {
 
     live : function() {
-        this
-            .liveBindTo('trigger', 'leftclick', function(e) {
-                this.onTriggerClick(e);
-            })
+        this.__base();
     }
 
 });
