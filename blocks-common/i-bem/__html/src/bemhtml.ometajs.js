fc6a4531b7aa8b2fa640157cb7fe705e8feae384
Fedor Indutny <fedor.indutny@gmail.com>
2011-11-25 00:58:52 +0400

    [bemhtml] better compatibility fix

--- blocks-common/i-bem/__html/src/bemhtml.ometajs.js
+++ blocks-common/i-bem/__html/src/bemhtml.ometajs.js
@@ -25,7 +25,10 @@ var BSJSIdentity = ometajs_.BSJSIdentity;
 var BSJSTranslator = ometajs_.BSJSTranslator;
 var BSOMetaJSParser = ometajs_.BSOMetaJSParser;
 var BSOMetaJSTranslator = ometajs_.BSOMetaJSTranslator;
-(function(fail, OMeta) {{
+if (global === ometajs_) {
+  fail = function() { return fail };
+  OMeta = require('ometajs').OMeta;
+}{
     var BEMHTMLParser = exports.BEMHTMLParser = objectThatDelegatesTo(XJSTParser, {
         bemMatch: function() {
             var $elf = this, _fromIdx = this.input.idx;
@@ -428,4 +431,4 @@ var BSOMetaJSTranslator = ometajs_.BSOMetaJSTranslator;
             });
         }
     });
-}})(global === ometajs_ ? function() { return fail } : fail,global === ometajs_ ? require('ometajs').OMeta : OMeta);
+}
