69c07e321f829e610ff133ef725411b6ec01b4da
Varvara Stepanova <toivonen@yandex-team.ru>
2012-02-12 22:06:47 +0400

    Building results

--- sets/common-desktop/b-icon/examples/20-b-icon_css.bemjson.js
+++ sets/common-desktop/b-icon/examples/20-b-icon_css.bemjson.js
@@ -2,8 +2,8 @@
     block: 'b-page',
     title: 'b-icon',
     head: [
-        { elem: 'css', url: '_20icon-css.css' },
-        { elem: 'js', url: '_20icon-css.js' }
+        { elem: 'css', url: '_20-b-icon_css.css' },
+        { elem: 'js', url: '_20-b-icon_css.js' }
     ],
     content: {
         block: 'b-icon',
---
+++ sets/common-desktop/b-icon/examples/30-b-icon_sprite.bemjson.js
@@ -0,0 +1,12 @@
+({
+    block: 'b-page',
+    title: 'b-icon',
+    head: [
+        { elem: 'css', url: '_30-b-icon_sprite.css' },
+        { elem: 'js', url: '_30-b-icon_sprite.js' }
+    ],
+    content: {
+        block: 'b-icon',
+        mods: { action: 'widgets' }
+    }
+})
