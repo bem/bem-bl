e59ea3cb9bd4984dac3cf79dc3fa479cd1762faa
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-03-11 15:52:16 +0400

    "bind" method's implementation added

--- blocks-common/i-bem/i-bem.deps.js
+++ blocks-common/i-bem/i-bem.deps.js
@@ -11,7 +11,8 @@
             ]
         },
         { block: 'i-ecma', elem: 'object' },
-        { block: 'i-ecma', elem: 'array' }
+        { block: 'i-ecma', elem: 'array' },
+        { block: 'i-ecma', elem: 'function' }
     ],
     shouldDeps: { elem: 'internal' }
 })
