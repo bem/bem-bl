fcc809682e4e4908b94cabb2bfd01819d1526c4a
deepsweet <kir@soulshine.in>
2012-04-18 20:03:30 +0300

    [touch] i-pressed-controller added

---
+++ blocks-touch/i-pressed-controller/i-pressed-controller.js
@@ -0,0 +1,52 @@
+BEM.DOM.decl('i-pressed-controller', {
+
+    onSetMod: {
+
+        js: function() {
+
+            this
+                .bindTo('pointermove', this._onPointerMove)
+                .bindTo('pointerup pointercancel', this._onPointerUp);
+
+        }
+
+    },
+
+    _onPointerDown: function(e) {
+
+        var that = this;
+
+        that._pressedTimer = setTimeout(function() {
+            if (!that._isMove) {
+                that.setMod('pressed', 'yes');
+            }
+        }, 80);
+
+    },
+
+    _onPointerMove: function() {
+
+        this._isMove = true;
+
+    },
+
+    _onPointerUp: function() {
+
+        this.delMod('pressed');
+        this._isMove = false;
+
+        clearTimeout(this._pressedTimer);
+
+    }
+
+},{
+
+    live: function() {
+
+        this.liveBindTo('pointerdown', function() {
+            this._onPointerDown();
+        });
+
+    }
+
+});
