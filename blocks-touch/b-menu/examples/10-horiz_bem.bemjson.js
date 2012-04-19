6a884b0fa0e4097ad18fd8f21bbebf0cc4479a38
deepsweet <kir@soulshine.in>
2012-04-19 13:49:47 +0300

    [touch] b-menu added

---
+++ blocks-touch/b-menu/examples/10-horiz_bem.bemjson.js
@@ -0,0 +1,59 @@
+({
+    block: 'b-page',
+    title: 'заголовок',
+    head: [
+        {
+            elem: 'css',
+            url: '_10-horiz_bem.css'
+        },
+        {
+            elem: 'css',
+            ie: true,
+            url: '_10-horiz_bem.ie.css'
+        },
+        {
+            block: 'i-jquery',
+            elem: 'core'
+        },
+        {
+            elem: 'js',
+            url: '_10-horiz_bem.pub.js'
+        }
+    ],
+    content: {
+        block: 'b-menu',
+        mods: {
+            layout: 'horiz'
+        },
+        content: [
+            {
+                elem: 'item',
+                elemMods: {
+                    state: 'current'
+                },
+                content: 'Главная'
+            },{
+                elem: 'item',
+                content: {
+                    block: 'b-link',
+                    url: '#',
+                    content: 'Продукты'
+                }
+            },{
+                elem: 'item',
+                content: {
+                    block: 'b-link',
+                    url: '#',
+                    content: 'Поддержка'
+                }
+            },{
+                elem: 'item',
+                content: {
+                    block: 'b-link',
+                    url: '#',
+                    content: 'О компании'
+                }
+            }
+        ]
+    }
+})
