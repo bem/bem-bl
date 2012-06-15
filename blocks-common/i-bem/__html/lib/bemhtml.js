23aef9b8578c2fa9580dfb436e88bc27e982aeb2
Fedor Indutny <fedor.indutny@gmail.com>
2012-06-15 15:02:42 +0400

    bemhtml: refactor, add **usable** tests
    * bemhtml: move compilation stuff into lib/bemhtml.js

---
+++ blocks-common/i-bem/__html/lib/bemhtml.js
@@ -0,0 +1,59 @@
+var ometajs = require('ometajs'),
+    xjst = require('xjst'),
+    vm = require('vm'),
+    bemhtml = require('./ometa/bemhtml'),
+    BEMHTMLParser = bemhtml.BEMHTMLParser,
+    BEMHTMLToXJST = bemhtml.BEMHTMLToXJST;
+
+//
+// ### function translate (source)
+// #### @source {String} BEMHTML Source code
+// #### @options {Object} Compilation options **optional**
+// Returns source translated to javascript
+//
+exports.translate = function translate(source, options) {
+  var tree = BEMHTMLParser.matchAll(source, 'topLevel'),
+      xjstSources = BEMHTMLToXJST.match(tree, 'topLevel');
+
+  options || (options = {});
+
+  try {
+    var xjstTree = xjst.parse(xjstSources);
+  } catch (e) {
+    throw new Error("xjst parse failed");
+  }
+
+  try {
+    var xjstJS = (process.env.BEMHTML_ENV == 'development' || options.devMode) ?
+                   xjst.compile(xjstTree, '', { 'no-opt': true })
+                   :
+                   xjst.compile(xjstTree);
+  } catch (e) {
+      throw new Error("xjst to js compilation failed:\n" + e.stack);
+  }
+
+  return 'var BEMHTML = ' + xjstJS + ';\n' +
+         'BEMHTML = (function(xjst) {\n' +
+         '  return function() {\n' +
+         '    return xjst.apply.call(\n' +
+         (options.raw ? 'this' : '[this]') + '\n' +
+         '    ); };\n' +
+         '}(BEMHTML));\n' +
+         'typeof exports === "undefined" || (exports.BEMHTML = BEMHTML);';
+};
+
+//
+// ### function compile (source)
+// #### @source {String} BEMHTML Source code
+// #### @options {Object} Compilation options **optional**
+// Returns generator function
+//
+exports.compile = function compile(source, options) {
+  var body = exports.translate(source, options),
+      context = { exports: {} };
+
+  if (options && options.devMode) context.console = console;
+  vm.runInNewContext(body, context);
+
+  return context.BEMHTML;
+};
--- blocks-common/i-bem/bem/techs/bemhtml.js
+++ blocks-common/i-bem/bem/techs/bemhtml.js
@@ -2,7 +2,6 @@ var BEM = require('bem'),
     Q = BEM.require('q'),
     PATH = require('path'),
     SYS = require('util'),
-    XJST = require('xjst'),
 
     readFile = BEM.require('./util').readFile;
 
@@ -36,52 +35,8 @@ exports.getBuildResult = function(prefixes, suffix, outputDir, outputName) {
             sources = sources.join('\n');
 
             var BEMHTML = require('../../__html/lib/bemhtml');
-            try {
-                var tree = BEMHTML.BEMHTMLParser.matchAll(
-                    sources,
-                    'topLevel',
-                    undefined,
-                    function(m, i) {
-                        console.log(arguments);
-                        throw { errorPos: i, toString: function() { return "bemhtml match failed" } }
-                    });
 
-                var xjstSources = BEMHTML.BEMHTMLToXJST.match(
-                    tree,
-                    'topLevel',
-                    undefined,
-                    function(m, i) {
-                        console.log(arguments);
-                        throw { toString: function() { return "bemhtml to xjst compilation failed" } };
-                    });
-            } catch (e) {
-                e.errorPos != undefined &&
-                    SYS.error(
-                        sources.slice(0, e.errorPos) +
-                        "\n--- Parse error ->" +
-                        sources.slice(e.errorPos) + '\n');
-                console.log('error: ' + e);
-                throw e;
-            }
-
-            try {
-                var xjstTree = XJST.parse(xjstSources);
-            } catch (e) {
-                throw new Error("xjst parse failed");
-            }
-
-            try {
-                var xjstJS = process.env.BEMHTML_ENV == 'development' ?
-                    XJST.XJSTCompiler.match(xjstTree, 'topLevel') :
-                    XJST.compile(xjstTree);
-            } catch (e) {
-                throw new Error("xjst to js compilation failed");
-            }
-
-            return 'var BEMHTML = ' + xjstJS + '\n'
-                + 'BEMHTML = (function(xjst) { return function() { return xjst.apply.call([this]); }; }(BEMHTML));\n'
-                + 'typeof exports === "undefined" || (exports.BEMHTML = BEMHTML);';
-
+            return BEMHTML.translate(sources);
         });
 
 };
