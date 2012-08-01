a9d11542d8fdabb6da2558af817beb0a2cadbc8d
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-08-01 15:37:51 +0400

    i-bem: Don't destruct whole block with many dom-nodes on destruct one of them

--- blocks-desktop/i-bem/tests/10-base_bem.blocks/i-bem/__destruct/i-bem__destruct.test.js
+++ blocks-desktop/i-bem/tests/10-base_bem.blocks/i-bem/__destruct/i-bem__destruct.test.js
@@ -15,11 +15,32 @@ BEM.TEST.decl({ block : 'i-bem', elem : 'destruct' }, function() {
                 '<div class="b-test-destruct i-bem" onclick="return {\'b-test-destruct\':{id:\'b-test-destruct\'}}"/>' +
             '</div>'));
 
-        BEM.DOM.destruct(true, rootNode.find('.b-test-destruct:eq(0)'));
+        BEM.DOM.destruct(rootNode.find('.b-test-destruct:eq(0)'));
         expect(spy).not.toHaveBeenCalled();
 
-        BEM.DOM.destruct(true, rootNode.find('.b-test-destruct:eq(1)'));
+        BEM.DOM.destruct(rootNode.find('.b-test-destruct'));
         expect(spy).toHaveBeenCalled();
+    });
+
+    it('BEM.DOM.destruct should not destruct mixed block', function() {
+        var spy = jasmine.createSpy();
+        BEM.DOM.decl('b-test-mix-destruct', {
+            destruct : function() {
+                spy();
+                this.__base.apply(this, arguments);
+            }
+        });
+
+        BEM.DOM.decl('b-test-mix-destruct2', {});
+
+        var rootNode = BEM.DOM.init($(
+            '<div>' +
+                '<div class="b-test-mix-destruct b-test-mix-destruct2 i-bem" onclick="return {\'b-test-mix-destruct\':{id:\'b-test-mix-destruct\'},\'b-test-mix-destruct2\':{}}"/>' +
+                '<div class="b-test-mix-destruct i-bem" onclick="return {\'b-test-mix-destruct\':{id:\'b-test-mix-destruct\'}}"/>' +
+            '</div>'));
+
+        BEM.DOM.destruct(rootNode.find('.b-test-mix-destruct:eq(0)'));
+        expect(spy).not.toHaveBeenCalled();
     });
 
 });
