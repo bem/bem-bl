23cbc2572bd5f654d2b9d1c8009cd1a9434c1993
Sergey Belov <peimei@ya.ru>
2012-08-03 09:07:32 +0300

    Refactor js tech to new API,  add js-i tech

--- blocks-common/i-bem/bem/techs/js.js
+++ blocks-common/i-bem/bem/techs/js.js
@@ -1,42 +1,48 @@
 var BEM = require('bem'),
-    myPath = BEM.require('./path'),
-    Template = BEM.require('./template'),
-    Js = BEM.require('./legacy-techs/js');
+    Template = BEM.require('./template');
 
-exports.techModule = module;
+exports.baseTechPath = BEM.require.resolve('./techs/js.js');
 
-exports.outFile = Js.outFile;
+exports.techMixin = {
 
-exports.newFileContent = function (vars) {
-    vars.BemObj = 'BEM' + (/^i-/.test(vars.BlockName) ? '' : '.DOM');
-    vars.DeclObj = "'" + vars.BlockName + "'";
-    if (vars.ModName || vars.ModVal) {
-        vars.DeclObj = "{ block: " + vars.DeclObj +
-            (vars.ModName? ", modName: '" + vars.ModName + "'" : '') +
-            (vars.ModVal? ", modVal: '" + vars.ModVal + "'" : '') +
-            '}';
+    getCreateResult : function(path, suffix, vars) {
+
+        vars.BemObj = 'BEM' + (/^i-/.test(vars.BlockName) ? '' : '.DOM');
+        vars.DeclObj = "'" + vars.BlockName + "'";
+
+        if (vars.ModName || vars.ModVal) {
+            vars.DeclObj = "{ block: " + vars.DeclObj +
+                (vars.ModName? ", modName: '" + vars.ModName + "'" : '') +
+                (vars.ModVal? ", modVal: '" + vars.ModVal + "'" : '') +
+                '}';
+        }
+
+        return Template.process([
+            '/** @requires BEM */',
+            '/** @requires {{bemBemObj}} */',
+            '',
+            '(function(undefined) {',
+            '',
+            "{{bemBemObj}}.decl({{bemDeclObj}}, {",
+            '',
+            '    onSetMod : {',
+            '',
+            "        'js' : function() {",
+            '            /* ... */',
+            '        }',
+            '',
+            '    }',
+            '',
+            '}, {',
+            '',
+            '    live : function() {',
+            '        /* ... */',
+            '    }',
+            '',
+            '});',
+            '',
+            '})();'], vars);
+
     }
 
-    return Template.process([
-        '/** @requires {{bemBemObj}} */',
-        '',
-        '(function() {',
-        '',
-        "{{bemBemObj}}.decl({{bemDeclObj}}, {",
-        '',
-        '    onSetMod : {',
-        "        'js' : function() {",
-        '            /* ... */',
-        '        }',
-        '    }',
-        '',
-        '}, {',
-        '',
-        '    live : function() {',
-        '        /* ... */',
-        '    }',
-        '',
-        '});',
-        '',
-        '})();'], vars);
 };
