e179c86e2ea13698b6852749f195762da157d26b
Georgiy Mostolovitsa <hello@gfranco.ru>
2012-08-29 16:39:44 +0300

    Заменил index на i

--- blocks-common/i-bem/i-bem.js
+++ blocks-common/i-bem/i-bem.js
@@ -395,7 +395,7 @@ this.BEM = $.inherit($.observable, /** @lends BEM.prototype */ {
         }
 
         var _this = this;
-        $.each(elem || [undefined], function(index, elem) {
+        $.each(elem || [undefined], function(i, elem) {
             elem = elem && $(elem); // Если это элемент
             var modVal = _this.getMod(elem, modName);
             (modVal == modVal1 || modVal == modVal2) &&
