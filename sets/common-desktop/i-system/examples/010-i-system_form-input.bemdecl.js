dfaadd73e9220d004cc9ed6f424e5f3912686fc3
Varvara Stepanova <toivonen@yandex-team.ru>
2012-09-07 17:45:21 +0400

    Building result

---
+++ sets/common-desktop/i-system/examples/010-i-system_form-input.bemdecl.js
@@ -0,0 +1,21 @@
+exports.blocks = [
+    {
+        "name": "b-page"
+    },
+    {
+        "name": "b-form-input",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    "grey"
+                ]
+            }
+        ],
+        "elems": [
+            {
+                "name": "input"
+            }
+        ]
+    }
+]
---
+++ sets/common-desktop/i-system/examples/010-i-system_form-input.deps.js
@@ -0,0 +1,112 @@
+exports.deps = [
+    {
+        "block": "i-jquery"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "inherit"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "identify"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "is-empty-object"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "debounce"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "observable"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "object"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "array"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "function"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "i-bem",
+        "elem": "html"
+    },
+    {
+        "block": "i-ua"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "core"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom"
+    },
+    {
+        "block": "i-ecma",
+        "elem": "string"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init"
+    },
+    {
+        "block": "i-bem",
+        "elem": "dom",
+        "mod": "init",
+        "val": "auto"
+    },
+    {
+        "block": "b-page"
+    },
+    {
+        "block": "i-jquery",
+        "elem": "leftclick"
+    },
+    {
+        "block": "b-form-input"
+    },
+    {
+        "block": "i-system"
+    },
+    {
+        "block": "b-form-input",
+        "mod": "disabled"
+    },
+    {
+        "block": "b-form-input",
+        "mod": "disabled",
+        "val": "yes"
+    },
+    {
+        "block": "b-form-input",
+        "elem": "input"
+    },
+    {
+        "block": "b-form-input",
+        "mod": "theme"
+    },
+    {
+        "block": "b-form-input",
+        "mod": "theme",
+        "val": "grey"
+    }
+];
+exports.depsByTechs = {
+    "": {}
+};
