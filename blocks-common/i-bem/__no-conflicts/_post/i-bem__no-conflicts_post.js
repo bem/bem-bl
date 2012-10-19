8952b74cf12e37fc6803d4c51af452400fec6dce
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-10-19 20:27:31 +0400

    i-bem__no-conflicts: replace doc if exists

--- blocks-common/i-bem/__no-conflicts/_post/i-bem__no-conflicts_post.js
+++ blocks-common/i-bem/__no-conflicts/_post/i-bem__no-conflicts_post.js
@@ -1,3 +1,6 @@
 typeof BEM_VERSION !== 'undefined' && ((this._BEM || (this._BEM = {}))[this.BEM.version = BEM_VERSION] = this.BEM);
 
-typeof BEM_VERSION !== 'undefined' && (this._BEM[BEM_VERSION].DOM.doc = $('.bem-' + BEM_VERSION));
+if(BEM_VERSION !== 'undefined') {
+    var doc = $('.bem-' + BEM_VERSION);
+    doc[0] && (this._BEM[BEM_VERSION].DOM.doc = doc);
+}
