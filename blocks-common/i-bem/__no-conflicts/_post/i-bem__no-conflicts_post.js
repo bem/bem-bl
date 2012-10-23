022f1470fa4e86ecb9f099194af4918cd383df4c
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-10-23 14:27:01 +0400

    Fix normilizing doc

--- blocks-common/i-bem/__no-conflicts/_post/i-bem__no-conflicts_post.js
+++ blocks-common/i-bem/__no-conflicts/_post/i-bem__no-conflicts_post.js
@@ -1,6 +1,6 @@
 typeof BEM_VERSION !== 'undefined' && ((this._BEM || (this._BEM = {}))[this.BEM.version = BEM_VERSION] = this.BEM);
 
-BEM_VERSION !== 'undefined' && $(function() {
+typeof BEM_VERSION !== 'undefined' && $(function() {
     var doc = $('.bem-' + BEM_VERSION);
     doc[0] && (this._BEM[BEM_VERSION].DOM.doc = doc);
-});
+}.bind(this));
