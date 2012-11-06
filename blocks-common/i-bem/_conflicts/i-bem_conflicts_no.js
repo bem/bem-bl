969cbfdb3725c9532f25c6821281f6f535c260a5
Sergey veged Berezhnoy <veged@mail.ru>
2012-11-06 19:30:31 +0400

    Используем BEM.CONFLICTS_NO для хранения версии и переключения глобального рантайма

--- blocks-common/i-bem/_conflicts/i-bem_conflicts_no.js
+++ blocks-common/i-bem/_conflicts/i-bem_conflicts_no.js
@@ -1,1 +1,6 @@
-typeof BEM_CONFLICTS_NO !== 'undefined' && ((this._BEM || (this._BEM = {}))[this.BEM.version = BEM_CONFLICTS_NO] = this.BEM);
+typeof BEM_CONFLICTS_NO !== 'undefined' && (function(_this) {
+    var _BEM = _this._BEM || (_this._BEM = {});
+    _BEM[BEM_CONFLICTS_NO] = _this.BEM;
+    (_this.BEM.CONFLICTS_NO = function(version) { return _this.BEM = _BEM[version] })
+        .valueOf = function() { return BEM_CONFLICTS_NO };
+})(this);
