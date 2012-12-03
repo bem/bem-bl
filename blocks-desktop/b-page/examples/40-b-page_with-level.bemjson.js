4cd25706a5b6491e356758548db6f9c3a2cb2ff7
Vladimir Grinenko <i@tadatuta.com>
2012-12-03 15:26:27 +0200

    b-page: Fix typo in example

--- blocks-desktop/b-page/examples/40-b-page_with-level.bemjson.js
+++ blocks-desktop/b-page/examples/40-b-page_with-level.bemjson.js
@@ -1,6 +1,6 @@
 ({
     block: 'b-page',
-    title: 'Page with extra level of defenition',
+    title: 'Page with extra level of definition',
     head: [
         { elem: 'css', url: '_40-b-page_with-level.css' }
     ],
