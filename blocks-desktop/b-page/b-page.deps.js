d1f89d6e842b76d5b7756fc9ecfa28452a6aa33d
gela <gela-d@yandex-team.ru>
2012-12-24 19:47:14 +0200

    b-page: correct deps.js

--- blocks-desktop/b-page/b-page.deps.js
+++ blocks-desktop/b-page/b-page.deps.js
@@ -1,6 +1,6 @@
 ({
     mustDeps: [
-        { block: 'i-bem', elems: 'html' },
+        { block: 'i-bem', elems: ['html'] },
         { block: 'i-bem', elem: 'dom', mods: { init: 'auto' } },
         { block: 'i-ua' },
         { block: 'i-jquery', elems: 'core' }
