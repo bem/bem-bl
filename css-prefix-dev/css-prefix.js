2d5ccf0fac36e6bed236cee24cbfbab000c147c1
lostsoul <lostsoul@beyondtheclouds.net>
2013-01-16 16:31:30 +0000

    i-bem__dom_conflicts_no: css prefixes update

--- css-prefix-dev/css-prefix.js
+++ css-prefix-dev/css-prefix.js
@@ -1,21 +1,37 @@
 var cssp = require('cssp')
 
+function modifyRuleset(ruleset, prefix) {
+	if(!(ruleset instanceof Array)) return;
+	if(ruleset[0] != 'ruleset') return;
+	
+	ruleset[1].forEach(function(elem){ modifySelector(elem, prefix) });
+};
+
+function modifySelector(selector, prefix) {
+	if(!(selector instanceof Array)) return;
+	if(selector[0] != 'simpleselector') return;
+	
+	//console.log(selector);
+	var selectorType = selector.shift(),
+		selectorIndents = [];
+	while(selector[0][0] == 's'|| selector[0][0] == 'comment')
+		selectorIndents.push(selector.shift());
+
+	
+	selector.unshift(
+		['clazz', ['ident', prefix]],
+		['s', ' ']
+	);
+	selectorIndents.length && selector.unshift.apply(selector, selectorIndents);
+	selector.unshift(selectorType);
+
+	//console.log('*******************');
+	//console.log(selector);
+}
+
 module.exports.addPrefix = function(source, prefix) {
 	var tree = cssp.parse(source);
-	
-	tree.forEach(function(elem){
-		if(!(elem instanceof Array)) return;
-		if(elem[0] != 'ruleset') return;
-		if(elem[1][0] != 'selector') return;
-		
-		var selector = elem[1][1],
-			selectorType = selector.shift();
-		selector.unshift(
-			selectorType,
-			['clazz', ['ident', prefix]],
-			['s', ' ']
-		);
-	})
+	tree.forEach(function(elem){ modifyRuleset(elem, prefix) });
 
 	return cssp.translate(tree);
 };
--- css-prefix-dev/test-prefix.js
+++ css-prefix-dev/test-prefix.js
@@ -3,5 +3,9 @@ var fs = require('fs'),
 
 fs.readFile(process.argv[2], 'utf8', function(err, data) {
 	if(err) return console.log(err);
-	console.log(addPrefix(data, 'b-ololo'));
+	data = addPrefix(data, 'b-ololo');
+	fs.writeFile(process.argv[3], data, function(err) {
+		if(err) return console.log(err);
+		console.log("Job done");
+	});
 })
