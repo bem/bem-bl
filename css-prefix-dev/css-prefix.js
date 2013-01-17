57a77a6b39c08be6c20656ddf4b877c21a3c5fdc
lostsoul <lostsoul@beyondtheclouds.net>
2013-01-17 15:02:52 +0000

    i-bem__dom_conflicts_no: css prefixes complete

--- css-prefix-dev/css-prefix.js
+++ css-prefix-dev/css-prefix.js
@@ -1,37 +1,67 @@
 var cssp = require('cssp')
 
-function modifyRuleset(ruleset, prefix) {
+function modifyRuleset(ruleset, prefix, topmostClasses) {
 	if(!(ruleset instanceof Array)) return;
 	if(ruleset[0] != 'ruleset') return;
 	
-	ruleset[1].forEach(function(elem){ modifySelector(elem, prefix) });
+	ruleset[1].forEach(function(elem){ modifySelector(elem, prefix, topmostClasses) });
 };
 
-function modifySelector(selector, prefix) {
+function modifySelector(selector, prefix, topmostClasses) {
 	if(!(selector instanceof Array)) return;
 	if(selector[0] != 'simpleselector') return;
 	
-	//console.log(selector);
 	var selectorType = selector.shift(),
-		selectorIndents = [];
-	while(selector[0][0] == 's'|| selector[0][0] == 'comment')
-		selectorIndents.push(selector.shift());
-
+		firstPart = [],
+		topmostTags = ['html','body'],
+		prefixSpace = false;
 	
-	selector.unshift(
-		['clazz', ['ident', prefix]],
-		['s', ' ']
-	);
-	selectorIndents.length && selector.unshift.apply(selector, selectorIndents);
-	selector.unshift(selectorType);
+	//TODO: подумать что делать, когда на html реально надо повесить стиль в духе width: 100%
+	while(true) {
+		if(!selector[0]) break;
+		
+		var elem = selector[0];
+			elemType = selector[0][0]
 
-	//console.log('*******************');
-	//console.log(selector);
+		if(elemType == 's' && selector.length != 1 || elemType == 'comment') {
+			firstPart.push(selector.shift());
+			continue;
+		}
+		
+		if((elemType == 'ident' && topmostTags.indexOf(elem[1]) != -1) ||
+		   (elemType == 'clazz' && topmostClasses.indexOf(elem[1][1]) != -1)) {
+			firstPart.push(selector.shift());
+			elemType == 's' && selector.length != 1 ? firstPart.push(selector.shift()) : (prefixSpace = true)
+			continue;
+		}
+
+		break;
+	}
+
+	/*
+	console.log(selector);
+	console.log(selector.legnth != 0);
+	console.log(selector.length == 1 && selector[0][0] == 'ident');
+	console.log('###########');
+	*/
+
+	if(selector.legnth != 0 && !(selector.length == 1 && selector[0][0] == 's'))
+		selector.unshift(['s', ' ']);
+	
+	selector.unshift(['clazz', ['ident', prefix]]);
+	
+	if(prefixSpace)
+		selector.unshift(['s', ' ']);
+	
+	if(firstPart.length)
+		selector.unshift.apply(selector, firstPart);
+	
+	selector.unshift(selectorType);
 }
 
-module.exports.addPrefix = function(source, prefix) {
+module.exports.addPrefix = function(source, prefix, topmostClasses) {
 	var tree = cssp.parse(source);
-	tree.forEach(function(elem){ modifyRuleset(elem, prefix) });
+	tree.forEach(function(elem){ modifyRuleset(elem, prefix, topmostClasses) });
 
 	return cssp.translate(tree);
 };
--- css-prefix-dev/test-prefix.js
+++ css-prefix-dev/test-prefix.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
 
 fs.readFile(process.argv[2], 'utf8', function(err, data) {
 	if(err) return console.log(err);
-	data = addPrefix(data, 'b-ololo');
+	data = addPrefix(data, 'b-all-done', ['i-ua_js_yes', 'i-ua_js_no']);
 	fs.writeFile(process.argv[3], data, function(err) {
 		if(err) return console.log(err);
 		console.log("Job done");
