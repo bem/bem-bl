46b707b2c363b2643dbe983bea01cc464ab7f917
Guro Bokum <jiojiajiu@gmail.com>
2013-02-04 05:01:58 +0400

    i18n.js.js many keys

--- blocks-common/i-bem/__i18n/lib/i18n-js.js
+++ blocks-common/i-bem/__i18n/lib/i18n-js.js
@@ -1,4 +1,5 @@
 var TANKER = require('./tanker');
+var BEM = require('bem');
 
 exports.serializeAllData = function(data, langs, defaultLang) {
 
@@ -12,34 +13,61 @@ exports.serializeAllData = function(data, langs, defaultLang) {
 };
 
 exports.serializeData = function(data, lang) {
-
     var res = [];
+    var KEYS = (process.env.BEM_I18N_KEYS || []);
 
     Object.keys(data).sort().forEach(function(keyset) {
-
+        var keysetData = data[keyset];
         // output value of empty keyset as a simple js code
         if (keyset === '') {
-            res.push(data[keyset]);
+            res.push(keysetData);
             return;
-        }
+        };
 
-        // generate i18n declaration for normal keysets
-        res.push("\nBEM.I18N.decl('" + keyset + "', {");
-
-        Object.keys(data[keyset]).forEach(function(key, i, arr) {
-
-            TANKER.xmlToJs(data[keyset][key], function(js) {
-                res.push(JSON.stringify(key) + ': ' + js + (i === arr.length - 1 ? '' : ','));
-            });
-
-        });
-
-        res.push('}, {\n"lang": "' + lang + '"\n});\n');
+        getDecl(keysetData, keyset, { lang: lang }, res);
 
     });
 
     return res;
 
+    function getDecl (data, keyset, declKeys, res) {
+        var decl = [];
+
+        Object.keys(data).sort(function (a, b) {
+            // objects go first
+            if (typeof data[a] === 'object')
+                return -1;
+            return 1;
+        }).forEach(function (key, i, arr) {
+            var keyData = data[key];
+
+            console.log('typeof', typeof keyData, keyData);
+            if (typeof keyData === 'object') {
+                // BEM_I18N_KEYS
+                if (~KEYS.indexOf(key)) {
+                    Object.keys(keyData).sort().forEach(function (k) {
+                        // keyData[k] should be object
+                        if (typeof keyData[k] !== 'object')
+                            return;
+                        var extDeclKeys = BEM.util.extend(true, {}, declKeys);
+                        extDeclKeys[key] = k;
+                        getDecl(keyData[k], keyset, extDeclKeys, res);
+                    });
+                };
+            } else {
+                TANKER.xmlToJs(data[key], function(js) {
+                    decl.push(JSON.stringify(key) + ': ' + js + (i === arr.length - 1 ? '' : ','));
+                });
+            };
+        });
+
+        decl.unshift("\nBEM.I18N.decl('" + keyset + "', {");
+        decl.push('}, ');
+        decl.push(JSON.stringify(declKeys));
+        decl.push(');\n');
+
+        res = res.push(decl.join(''));
+    };
 };
 
 exports.serializeInit = function(lang) {
