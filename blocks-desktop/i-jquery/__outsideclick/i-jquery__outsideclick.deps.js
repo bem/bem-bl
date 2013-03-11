2a13af2c2e3a9cac1c007f1a9dd7b44500991a1d
Misha Berezin <mishaberezin@gmail.com>
2013-03-11 22:04:46 +0200

    Расширение для события "outsideclick".

---
+++ blocks-desktop/i-jquery/__outsideclick/i-jquery__outsideclick.deps.js
@@ -0,0 +1,5 @@
+({
+    shouldDeps: [
+        { block: 'i-ecma', elem: 'function' }
+    ]
+})
---
+++ blocks-desktop/i-jquery/__outsideclick/i-jquery__outsideclick.js
@@ -0,0 +1,33 @@
+(function($) {
+
+var outsideClick = $.event.special.outsideclick = {
+
+    add: function(obj) {
+
+        $(document).on('click.' + obj.guid, outsideClick.handler.bind(this));
+
+    },
+
+    remove: function(obj) {
+
+        $(document).off('click.' + obj.guid);
+
+    },
+
+    handler: function(e) {
+
+        if(!$.contains(this, e.target)) {
+
+            e.type = 'outsideclick';
+
+            $.event.handle.apply(this, arguments);
+
+            e.type = 'click';
+
+        }
+
+    }
+
+};
+
+})(jQuery);
