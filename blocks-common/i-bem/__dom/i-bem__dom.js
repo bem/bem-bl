a97f8127dbb766f0699ec99cab47ae135180fbfe
Andrey Smirnov <smirnovandr@yandex.ru>
2013-05-27 14:14:25 +0400

    Update i-bem__dom.js
    IE8 триггерит resize окна при изменении размеров любого элемента, иногда это может привести к неожиданным результатам
    http://cyanbyfuchsia.wordpress.com/2012/06/20/window-resize-and-internet-explorer-8/

--- blocks-common/i-bem/__dom/i-bem__dom.js
+++ blocks-common/i-bem/__dom/i-bem__dom.js
@@ -444,6 +444,34 @@ var DOM = BEM.DOM = BEM.decl('i-bem__dom',/** @lends BEM.DOM.prototype */{
      */
     bindToWin : function(event, fn) {
 
+        var _fn = fn,
+            currentHeight,
+            currentWidth,
+            win;
+
+        if (event === 'resize') {
+
+            win = $(window);
+
+            fn = function() {
+
+                var height = win.height(),
+                    width = win.width();
+
+                if (currentHeight !== height || currentWidth !== width) {
+
+                    currentHeight = height;
+                    currentWidth = width;
+
+                    _fn.apply(this, arguments);
+
+                }
+
+
+            }
+
+        }
+
         this._needSpecialUnbind = true;
         return this.bindToDomElem(win, event, fn);
 
