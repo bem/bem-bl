81c7a016b8cf3edd4ff22b8658f0ee529a050149
Oleg Apostol <apostol@yandex-team.ru>
2013-12-23 16:03:30 +0200

    i-jquery: Добавить плагин для события 'tap'

---
+++ blocks-touch/i-jquery/__tap/i-jquery__tap.js
@@ -0,0 +1,35 @@
+/**
+ * Плагин предназначен для эмуляции события tap для touch-платформ.
+ * Создан для сохранения обратной совместимости
+ * на проектах, использующих одноверменно islands-библиотек и romochka
+ */
+
+(function($) {
+
+var tap = $.event.special.tap = {
+
+    setup : function() {
+
+        $(this).bind('click', tap.handler);
+
+    },
+
+    teardown : function() {
+
+        $(this).unbind('click', tap.handler);
+
+    },
+
+    handler : function(e) {
+
+        if(!e.button) {
+            e.type = 'tap';
+            $.event.handle.apply(this, arguments);
+            e.type = 'click';
+        }
+
+    }
+
+};
+
+})(jQuery);
