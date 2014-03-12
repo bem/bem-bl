68669c49363624e3973e8e2631cf43d96508b75e
Alex Baumgertner <alexbaum@yandex-team.ru>
2014-03-12 15:37:43 +0200

    i-ua_interaction_yes

---
+++ blocks-desktop/i-ua/i-ua.examples/10-i-ua.blocks/b-page/b-page.deps.js
@@ -0,0 +1,7 @@
+({
+    mustDeps: [
+        { block: 'i-bem', elems: ['dom', 'i18n'] },
+        { block: 'i-jquery', elems: ['leftclick', 'tap'] },
+        { block: 'i-ua', mods: { interaction: 'yes' } }
+    ]
+})
