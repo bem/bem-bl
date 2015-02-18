800c1bf0b0b762d189ab805903af3aa89757c6f6
restrry <restrry@yandex-team.ru>
2015-02-18 19:52:56 +0300

    b-page deps fix

--- blocks-common/b-page/b-page.deps.js
+++ blocks-common/b-page/b-page.deps.js
@@ -1,6 +1,8 @@
 ({
     mustDeps: [
+        { block: 'i-bem', elems: ['html'] },
         { block: 'i-bem', elem: 'dom', mods: { init: 'auto' } },
+        { block: 'i-ua' },
         { block: 'i-jquery', elems: 'core' }
     ],
     shouldDeps: { elems: 'js' }
--- blocks-desktop/b-page/b-page.deps.js
+++ blocks-desktop/b-page/b-page.deps.js
@@ -1,7 +1,3 @@
 ({
-    mustDeps: [
-        { block: 'i-bem', elems: ['html'] },
-        { block: 'i-ua' }
-    ],
     shouldDeps: {elems: ['css']}
 });
